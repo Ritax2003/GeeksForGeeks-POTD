c5247289dbd364de2b5e18bee562ed74c1a913d6
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-02-18 18:21:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Last modified ball/last-modified-ball.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int solve(int N, vector<int> A) {
+        int flage=0;
+
+        for(int i=N-1;i>=0;i--){
+
+            if(A[i]!=9){
+
+                flage = i+1;
+
+                break;
+
+            }
+
+        }
+
+        return flage;
+
+    
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        vector<int> A(N);
+
+        for (int i = 0; i < N; i++) cin >> A[i];
+
+        Solution ob;
+        cout << ob.solve(N, A) << "\n";
+    }
+    return 0;
+}
+    
+// } Driver Code Ends
