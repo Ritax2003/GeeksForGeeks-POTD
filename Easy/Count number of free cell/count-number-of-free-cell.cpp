7725d01920b63d82a95e5251b60ea52c3988517c
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-02-18 18:31:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count number of free cell/count-number-of-free-cell.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+  public:
+  vector<long long int> countZero(int n, int k, vector<vector<int>>& arr){
+       vector<long long> ans;
+        int rk = 0, ck = 0;
+        long long temp = n*n;
+        int row[100001] = {0};
+        int col[100001] = {0};
+        for(int i = 0;i<k;i++)
+        {
+            int r = arr[i][0], c = arr[i][1];
+            if(row[r] == 1 && col[c] == 1)
+            {
+                ans.push_back(temp);
+            }
+            else if(row[r] == 1)
+            {
+                col[c] = 1;
+                ck++;
+                temp = temp - n + rk;
+                ans.push_back(temp);
+            }
+            else if(col[c] == 1)
+            {
+                row[r] = 1;
+                rk++;
+                temp = temp - n + ck;
+                ans.push_back(temp);
+            }
+            else
+            {
+                row[r] = 1;
+                col[c] = 1;
+                temp = temp - n + ck - n + rk + 1;
+                ck++;
+                rk++;
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<vector<int>> arr(k,vector<int>(2));
+        for(int i=0;i<k;i++){
+            int x, y;
+            cin>>x>>y;
+            arr[i][0] = x;
+            arr[i][1] = y;
+        }
+        Solution ob;
+        vector<long long int> ans = ob.countZero(n,k,arr);
+        
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;    
+    }
+    
+    return 0;
+}
+
+
+// } Driver Code Ends
