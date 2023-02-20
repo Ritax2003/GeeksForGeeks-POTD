3aebf3c734811a99a89c4e0bbb1b5a034513e083
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-02-21 00:05:48 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Fill the Matrix/fill-the-matrix.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int minIteration(int n, int m, int x, int y){    
+        int ans=max(max(x-1+y-1,x-1+m-y),max(n-x+y-1,n-x+m-y ));
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, M, x, y;
+        cin >> N >> M;
+        cin >> x >> y;
+        
+        Solution ob;
+        cout << ob.minIteration(N, M, x, y) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
