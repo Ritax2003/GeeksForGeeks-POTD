d10cd9137e54a39d0851e4032befcffa144ab194
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-03-03 00:08:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Cutting Rectangles/cutting-rectangles.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        long long int k = __gcd(L,B);
+
+        long long int ans = (L*B)/(k*k);
+
+        return {ans,k};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int L, B;
+        cin>>L>>B;
+        
+        Solution ob;
+        vector<long long int> ans = ob.minimumSquares(L, B);
+        cout<<ans[0]<<" "<<ans[1]<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
