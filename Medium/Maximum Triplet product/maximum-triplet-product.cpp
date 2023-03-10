7fdaa0d800a436316fb4ee53017e9b3b506d5e93
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-03-10 06:54:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Triplet product/maximum-triplet-product.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for CPP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long maxTripletProduct(long long arr[], int n)
+    {
+      sort(arr,arr+n);
+
+     
+
+     long long p1 = arr[0]*arr[1]*arr[n-1]; // two negative number 
+
+     
+
+     long long p2 = arr[n-1]*arr[n-2]*arr[n-3]; // all poisitive number
+
+     
+
+     return max(p1,p2);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,i;
+	    cin>>n; long long arr[n];
+	    for(i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution ob;
+	    cout <<ob.maxTripletProduct(arr, n);
+	    cout<<"\n";
+	}
+    return 0;
+}
+// } Driver Code Ends
