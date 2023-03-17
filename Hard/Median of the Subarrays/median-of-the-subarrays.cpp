8f4fcd7f6da68d44395b1f0cfe10104a8dbba9aa
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-03-18 00:17:35 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Median of the Subarrays/median-of-the-subarrays.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution {
+  public:
+     long long solve(int n, vector<int>&ans,  int m){
+        
+        // int mp[2*n+1];
+        // memset(mp,0,sizeof(mp));
+        vector<int>mp(2*n + 1, 0);
+        
+        long long cur=n, tot=0, sum=0;
+        mp[cur]++;
+        
+        for(int i=0;i<n;i++){
+          int x= -1;
+          if(ans[i]>=m)
+            x=1;
+          if(x==-1){
+            tot-=mp[cur+x];
+          }
+          else{
+            tot+=mp[cur];
+          }
+          cur+=x;
+          sum+=tot;
+          mp[cur]++;
+        }
+        
+        return sum;
+  }
+ 
+    long long countSubarray(int N, vector<int> A,int M) {
+        return solve(N, A, M) - solve(N, A, M+1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin>>N>>M;
+        vector<int> A(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        Solution obj;
+        auto ans = obj.countSubarray(N,A,M);
+        cout<<ans<<endl;
+    }
+}  
+// } Driver Code Ends
