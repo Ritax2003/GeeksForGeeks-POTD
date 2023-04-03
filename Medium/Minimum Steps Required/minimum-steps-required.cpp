49c359229fd4d6ff14d7558a89143b69bb07dda1
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-04-04 00:34:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum Steps Required/minimum-steps-required.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+int minSteps(string str) {
+        int ans = 1;
+    for(int i = 0; i < str.size()-1;i++)
+        if(str[i] != str[i+1])
+            ans++;
+    return ans/2+1;
+}
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        cout << minSteps(str) << endl;
+    }
+}
+
+// } Driver Code Ends
