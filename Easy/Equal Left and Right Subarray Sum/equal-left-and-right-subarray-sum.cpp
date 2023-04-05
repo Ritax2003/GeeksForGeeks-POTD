5a70bd07618b56a4aa8738e0ee76507b71f3065a
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-04-06 00:04:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Equal Left and Right Subarray Sum/equal-left-and-right-subarray-sum.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int equalSum(int N, vector<int> &A) {
+        int sum = 0;
+        int total = 0;
+        for(int i=0;i<A.size();i++){
+            total += A[i];
+        }
+        for(int i=0;i<A.size();i++){
+            total -= A[i];
+            if(sum==total){
+                return i+1;
+            }
+            sum += A[i];
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N; 
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.equalSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
