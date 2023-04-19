9bab4189f77a3dfe3f68605272580b88d250a713
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-04-19 08:53:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Wifi Range/wifi-range.cpp
+++ Easy/Wifi Range/wifi-range.cpp
@@ -10,22 +10,59 @@ using namespace std;
 
 class Solution{
     public:
-     bool wifiRange(int n, string s, int x){
+    bool wifiRange(int N, string S, int X){
         // code here
-        int t=0;
-        for(int i=0; i<n; i++){
-            if(s[i] == '1'){
-                if(x >= abs(t)) t = x;
-                else return 0;
-            }
-            else
-            {
-                t--;
+    // Convert the input string S to a character array wifiCoverage
+    char wifiCoverage[N];
+    strcpy(wifiCoverage, S.c_str());
+    
+    // Loop through the wifiCoverage array
+    for (int i = 0; i < N; i++) {
+        // If there is a wifi in the ith room
+        if (S[i] == '1') {
+            // Loop through the range of rooms covered by the wifi
+            for (int j = i - X; j <= i + X; j++) {
+                // Mark all the corresponding rooms in the wifiCoverage array
+                if (j >= 0 && j < N) {
+                    wifiCoverage[j] = '1';
+                }
             }
         }
-        if(t<0) return 0;
-        return 1;
     }
+    
+    // Loop through the wifiCoverage array
+    for (int i = 0; i < N; i++) {
+        // If there is a room that is not covered by any wifi, return false
+        if (wifiCoverage[i] == '0') {
+            return false;
+        }
+    }
+    
+    // If all rooms are covered by a wifi, return true
+    return true;
+
+    }
+        /*
+        2nd soln
+        bool wifiRange(int N, string S, int X){
+        string a=S;
+        for(int i=0;i<N;i++){
+            if(S[i]=='1'){
+                for(int j=i-X;j<=i+X;j++){
+                    if(j>=0 && j<N){
+                        a[j]=1;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<N;i++){
+            if(a[i]=='0'){
+                return false;
+            }
+        }
+        return true;
+
+    }*/
 };
 
 //{ Driver Code Starts.
