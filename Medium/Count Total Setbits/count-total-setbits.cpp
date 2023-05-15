507d607945292af7789bca1fd8fc1e7b4478b6eb
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-05-15 06:40:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count Total Setbits/count-total-setbits.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long countBits(long long N) {
+        long long c = 0;
+        for(int i = 1; i<=N; i++){
+            c+=__builtin_popcountll(i);
+        }
+        return c;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        long long N;
+        scanf("%lld",&N);
+        
+        Solution obj;
+        long long res = obj.countBits(N);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
