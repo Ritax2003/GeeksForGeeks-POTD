e5703611c9dbad2e8b69d85ee25c5e8278b5184d
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-06-14 00:13:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum Diamonds/maximum-diamonds.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long maxDiamonds(int a[], int n, int k) {
+       priority_queue<int>q;
+        for(int i=0;i<n;i++){
+            q.push(a[i]);
+        }
+        long long ans=0;
+        while(k--){
+            ans+=q.top();
+             q.push(q.top()/2);
+            q.pop();
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+        
+        cin>>N>>K;
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.maxDiamonds(A,N,K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
