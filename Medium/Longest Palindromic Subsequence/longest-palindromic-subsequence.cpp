c0d33a2ae40e84cecafca30e8b2f1ab2bde7705a
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-07-19 00:18:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    int longestPalinSubseq(string A) {
+         int n=A.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(A[i-1]==A[n-j]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][n];
+    }
+};
+
+//{ Driver Code Starts.
+
+int32_t main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        cout << ob.longestPalinSubseq(s) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
