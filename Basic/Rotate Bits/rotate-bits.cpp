d1e742bf79293ba81fa2e71c2e5cebdd07a504c1
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-09-20 08:03:13 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Rotate Bits/rotate-bits.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+        vector <int> rotate (int n, int d){
+          d=d%16;
+            int l=(n<<d)|(n>>(16-d));
+            int r=(n>>d)|(n<<(16-d));
+            int mask=(1<<16)-1;
+            l&=mask;    //taking only the first 16 bits
+            r&=mask;    //taking only the first 16 bits
+            return {l,r};
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        
+        int n, d; cin >> n >> d;
+        Solution ob;
+        vector <int> res = ob.rotate (n, d);
+        cout << res[0] << endl << res[1] << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
