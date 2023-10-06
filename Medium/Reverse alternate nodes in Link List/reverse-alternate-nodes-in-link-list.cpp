c588086fbad57bfe575d587e1a8a4cc8240f4750
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-10-06 07:10:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse alternate nodes in Link List/reverse-alternate-nodes-in-link-list.cpp
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    void rearrange(struct Node *odd){
+         if(odd->next == NULL || odd->next->next == NULL) return;
+        Node *curr = odd;
+        int cnt = 1;
+        while(curr->next){
+            curr = curr->next;
+            cnt++;
+        }
+       Node *first = odd;
+       Node *prev = NULL;
+       Node *second = first->next;
+       while(first->next != NULL && second->next != NULL){
+           first->next = second->next;
+           first = second->next;
+           second->next = prev;
+           prev = second;
+           second = first->next;
+       }
+      if(cnt%2 == 0){
+      second->next = prev;
+      }
+      else{
+          first->next = prev;
+      }
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
