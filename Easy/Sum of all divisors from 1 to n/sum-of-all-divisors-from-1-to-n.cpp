20397d5086ded818d8b20bfcfaf4ca78589fc7fa
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-10-21 07:16:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sum of all divisors from 1 to n/sum-of-all-divisors-from-1-to-n.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+            long long sum=0;
+        
+        for(int i=1 ; i<=N ; i++)
+         {
+             sum+= i*(N/i) ;
+         }
+         return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
