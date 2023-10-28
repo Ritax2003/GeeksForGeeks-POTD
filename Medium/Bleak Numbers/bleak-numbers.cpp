47632745c80cdadf129f51c80c3c1da1bb9c7717
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-10-28 07:44:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Bleak Numbers/bleak-numbers.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	int is_bleak(int n)
+	{
+	    for(int i=1;i<=32;i++){
+        if(n-i>0 && __builtin_popcount(n-i)==i){
+            return 0;
+        }
+       }
+    
+    return 1;
+	}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	Solution ob;
+    	int ans = ob.is_bleak(n);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
