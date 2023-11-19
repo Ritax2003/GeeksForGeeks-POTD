292890d7e11a4a6a6a462da9bea095a896117ea1
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-11-19 08:27:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Intersection of two sorted Linked lists/intersection-of-two-sorted-linked-lists.cpp
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    if(n)
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    else cout<< " ";
+}
+
+
+// } Driver Code Ends
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+     Node* findIntersection(Node* head1, Node* head2)
+    {
+      Node* temp=new Node(-1);
+      Node* sum=temp;
+      while(head1 && head2)
+      {
+          if(head1->data==head2->data)
+          {
+              sum->next=new Node(head2->data);
+              sum=sum->next;
+              head1=head1->next;
+              head2=head2->next;
+          }else if(head1->data>head2->data){
+              head2=head2->next;
+          }else{
+              head1=head1->next;
+          }
+      }
+      return temp->next; 
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Solution ob;
+	    Node* result = ob.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
