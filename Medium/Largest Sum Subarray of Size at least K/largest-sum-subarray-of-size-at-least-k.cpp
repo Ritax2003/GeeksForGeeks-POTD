13ba6c5a2d721f26978e4c79cdfcd74573a7e591
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-01-02 10:48:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Largest Sum Subarray of Size at least K/largest-sum-subarray-of-size-at-least-k.cpp
+++ Medium/Largest Sum Subarray of Size at least K/largest-sum-subarray-of-size-at-least-k.cpp
@@ -8,28 +8,26 @@ using namespace std;
 class Solution{
     public:    
     long long int maxSumWithK(long long int a[], long long int n, long long int k) {
-         long long int ans = INT_MIN;
+        long long int ans = INT_MIN;
         long long int sum = 0;
-        long long int last = 0;
-        long long int j = 0;
-        for(long long int i = 0; i < n; i++)
-        {
+        long long int start = 0;
+        long long prevele = 0;
+        
+        for(int i =0;i<n;i++){
             sum += a[i];
-            if(i - j + 1 == k)
-                ans = max(ans, sum);
-            else if(i - j + 1 > k)
-            {
-                last += a[j];
-                j++;
-                if(last < 0)
-                {
-                    sum -= last;
-                    last = 0;
+            if(i - start + 1 ==k) ans = max(ans,sum);
+            else if(i - start +1 >k){
+                prevele +=a[start];
+                start++;
+                if(prevele <0){
+                    sum-=prevele;
+                    prevele=0;
                 }
-                ans = max(ans, sum);
+                ans = max(ans,sum);
             }
         }
         return ans;
+        
     }
 };
 
