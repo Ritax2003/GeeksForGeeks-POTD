d93fbdbe7fe51d612a7fd8ba13ea4204539dc268
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-01-17 09:04:15 +0530

    Updated Solution - GfG to GitHub

--- Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.cpp
+++ Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.cpp
@@ -30,6 +30,17 @@ class Solution {
         }
     }
 };
+/* next permutation
+vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        vector<vector<int>> ans ;
+        sort(arr.begin(),arr.end());
+        do{
+            ans.push_back(arr);
+        }while(next_permutation(arr.begin(),arr.end()));
+        return ans ;
+    }
+
+*/
 
 //{ Driver Code Starts.
 
