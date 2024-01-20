bb2b323d93df1dce56a0ab4577617eae15163ef7
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-01-20 14:08:33 +0530

    Updated Solution - GfG to GitHub

--- Hard/Distribute candies in a binary tree/distribute-candies-in-a-binary-tree.cpp
+++ Hard/Distribute candies in a binary tree/distribute-candies-in-a-binary-tree.cpp
@@ -97,23 +97,19 @@ struct Node {
 class Solution
 {
     public:
-    int distribute_helper(Node* root,int& num_moves)
-    {
-        if(!root)
-        return 0;
-        
-        int left=distribute_helper(root->left,num_moves);
-        int right=distribute_helper(root->right,num_moves);
-        
-        num_moves+=abs(left)+abs(right);
-        
-        return root->key+left+right-1;
+    public:
+    int ans = 0;
+    int dfs(Node *root) {
+        if(!root) return 0;
+        int LHS = dfs(root -> left);
+        int RHS = dfs(root -> right);
+        ans += abs(LHS) + abs(RHS);
+        return root -> key + LHS + RHS - 1;
     }
     int distributeCandy(Node* root)
     {
-        int num_moves=0;
-        int call=distribute_helper(root,num_moves);
-        return num_moves;
+        dfs(root);
+        return ans;
     }
 };
 
