67ac00c11523830e2a0b37b09383087637976f37
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-01-23 08:45:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Course Schedule/course-schedule.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+  public:
+    vector<int> findOrder(int n, int m, vector<vector<int>> prerequisites) 
+    {
+        vector<vector<int>> adj(n, vector<int>());
+        vector<int> inDegree(n, 0);
+
+        // Build adjacency list and calculate in-degrees
+        for (int i = 0; i < m; ++i) {
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
+            adj[u].push_back(v);
+            inDegree[v]++;
+        }
+
+        // Create a queue for topological sorting
+        queue<int> q;
+        for (int i = 0; i < n; ++i) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> result;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            result.push_back(u);
+
+            for (int v : adj[u]) {
+                if (--inDegree[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+
+        // Check if there is a valid ordering
+        if (result.size() == n) {
+            return result;
+        } else {
+            // If not possible to finish all tasks, return an empty array
+            return {};
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        int u, v;
+
+        vector<vector<int>> prerequisites;
+
+        for (int i = 0; i < m; i++) {
+            cin >> u >> v;
+            prerequisites.push_back({u,v});
+        }
+        
+        vector<int> adj[n];
+        for (auto pre : prerequisites)
+            adj[pre[1]].push_back(pre[0]);
+        
+        Solution obj;
+        vector <int> res = obj.findOrder(n, m, prerequisites);
+        if(!res.size())
+            cout<<"No Ordering Possible"<<endl;
+        else
+            cout << check(n, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
