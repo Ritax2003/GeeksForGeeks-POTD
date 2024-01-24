7e8be8d685107dcaf49c8d416daf6f3e9d815061
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-01-24 17:53:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if a given graph is tree or not/check-if-a-given-graph-is-tree-or-not.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Matrix
+{
+public:
+    template <class T>
+    static void input(vector<vector<T>> &A,int n,int m)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                scanf("%d ",&A[i][j]);
+            }
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<vector<T>> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            for (int j = 0; j < A[i].size(); j++)
+            {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+   bool dfs(int node, int par, vector<int> adj[], vector<int>& vis){
+        vis[node] = 1;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                if(!dfs(it, node, adj, vis)) return false;
+            }else if(it != par) return false;
+        }
+        return true;
+    }
+    int isTree(int n, int m, vector<vector<int>> &edges) {
+        vector<int> adj[n];
+        for(auto it: edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> vis(n, 0);
+        bool fg = dfs(0, -1, adj, vis);
+        for(int i=0; i<n; i++){
+            if(!vis[i]) return false;
+        }
+        return fg;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n; 
+        scanf("%d",&n);
+        
+        
+        int m; 
+        scanf("%d",&m);
+        
+        
+        vector<vector<int>> edges(m, vector<int>(2));
+        Matrix::input(edges,m,2);
+        
+        Solution obj;
+        int res = obj.isTree(n, m, edges);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
